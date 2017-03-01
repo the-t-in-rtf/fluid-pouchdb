241bc9fad93e85302b61c67a18019e9ebf66922a
Tony Atkins <tony@raisingthefloor.org>
2017-03-01 13:49:39 +0100

    GPII-2296: Updated to use gpii-testem, and to compile cross-environment coverage reports.

---
+++ testem.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+"use strict";
+var fluid = require("infusion");
+var gpii  = fluid.registerNamespace("gpii");
+
+fluid.require("%gpii-testem");
+
+fluid.defaults("gpii.pouchdb.testem", {
+    gradeNames: ["gpii.testem.coverageDataOnly"],
+    testPages:  "tests/js/pouchdb-component-tests/html/index.html",
+    sourceDirs: ["src"],
+    coverageDir: "%gpii-pouchdb/coverage",
+    serveDirs:  ["src", "node_modules"],
+    testemOptions: {
+        "framework":   "qunit",
+        "parallel":    1
+    }
+});
+
+module.exports = gpii.pouchdb.testem().getTestemOptions();
